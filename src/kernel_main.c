4a4b205b7cafb57a3755c4a8bcdff61c99dcf4a1
amber <ambervanbree@gmail.com>
2025-07-19 16:56:39 +0200

    add printf

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -3,6 +3,7 @@
 #include "kernel.h"
 #include "screen.h"
 #include "string.h"
+#include "kprintf.h"
 #include "keyboard.h"
 
 
@@ -27,32 +28,34 @@ void kernel_main(void) //uint32_t magic, struct multiboot_info* mbi?
     screen_set_color(VGA_COLOR_LIGHT_GREEN, VGA_COLOR_BLACK);
     
     /* Display welcome message */
-    screen_putstring("Welcome to My Kernel!\n");
-    screen_putstring("Kernel from Scratch - KFS_1\n\n");
+    kprintf("Welcome to %s!\n", "KrnL");
+    kprintf("Kernel from Scratch - KFS_1\n\n");
     
     /* Display the required "42" */
     screen_set_color(VGA_COLOR_LIGHT_CYAN, VGA_COLOR_BLACK);
-    screen_putstring("The answer to everything: ");
-    screen_set_color(VGA_COLOR_WHITE, VGA_COLOR_BLACK);
-    screen_putstring("42\n\n");
+    kprintf("The answer to everything: %d\n", 42);
     
     /* Display some system information */
     screen_set_color(VGA_COLOR_BROWN, VGA_COLOR_BLACK);
-    screen_putstring("System Information:\n");
+    kprintf("System Information:\n");
     screen_set_color(VGA_COLOR_WHITE, VGA_COLOR_BLACK);
-    screen_putstring("- Architecture: i386 (x86)\n");
-    screen_putstring("- Boot loader: GRUB\n");
-    screen_putstring("- Kernel successfully loaded!\n\n");
+    kprintf("- Architecture: i386 (x86)\n");
+    kprintf("- Boot loader: GRUB\n");
+    kprintf("- Kernel successfully loaded!\n\n");
     
     /* Display success message */
     screen_set_color(VGA_COLOR_LIGHT_GREEN, VGA_COLOR_BLACK);
-    screen_putstring("Kernel initialization complete.\n");
-    screen_putstring("System ready!\n\n");
+    kprintf("Kernel initialization complete.\n");
+    kprintf("System ready!\n\n");
     
     /* Display keyboard instructions */
     screen_set_color(VGA_COLOR_LIGHT_CYAN, VGA_COLOR_BLACK);
-    screen_putstring("Keyboard input enabled!\n");
-    screen_putstring("Type to see characters appear on screen.\n\n");
+    kprintf("Keyboard input enabled!\n");
+    kprintf("Type to see characters appear on screen.\n\n");
+
+    int x = 12345;
+    kprintf("Address of x: %x\n", (unsigned int)&x);
+    kprintf("Value of x in hex: %x\n", x);
     
     /* Kernel main loop - interrupts will handle keyboard input */
     while (1) {
@@ -60,3 +63,4 @@ void kernel_main(void) //uint32_t magic, struct multiboot_info* mbi?
         asm volatile("hlt");
     }
 }
+    
---
+++ src/kprintf.c
@@ -0,0 +1,69 @@
+#include <stdarg.h>
+#include "screen.h"
+
+// Helper to print an integer in decimal
+static void print_decimal(int value) {
+    char buffer[12];
+    int i = 0, is_negative = 0;
+
+    if (value == 0) {
+        screen_putchar('0');
+        return;
+    }
+    if (value < 0) {
+        is_negative = 1;
+        value = -value;
+    }
+    while (value > 0) {
+        buffer[i++] = '0' + (value % 10);
+        value /= 10;
+    }
+    if (is_negative)
+        buffer[i++] = '-';
+    while (i--)
+        screen_putchar(buffer[i]);
+}
+
+static void print_hex(unsigned int value) {
+    int i = 0;
+    if (value == 0) {
+        screen_putstring("0x0");
+        return;
+    }
+    screen_putstring("0x");
+    for (i = 7; i >= 0; i--) {
+        int nibble = (value >> (i * 4)) & 0xF;
+        char c = (nibble < 10) ? ('0' + nibble) : ('A' + nibble - 10);
+        screen_putchar(c);
+    }
+}
+
+void kprintf(const char *fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+
+    for (; *fmt; ++fmt) {
+        if (*fmt == '%') {
+            ++fmt;
+            if (*fmt == 's') {
+                char *str = va_arg(args, char*);
+                screen_putstring(str);
+            } else if (*fmt == 'c') {
+                char c = (char)va_arg(args, int);
+                screen_putchar(c);
+            } else if (*fmt == 'd') {
+                int val = va_arg(args, int);
+                print_decimal(val);
+            } else if (*fmt == 'x') {
+                unsigned int val = va_arg(args, unsigned int);
+                print_hex(val);
+            } else if (*fmt == '%') {
+                screen_putchar('%');
+            }
+        } else {
+            screen_putchar(*fmt);
+        }
+    }
+
+    va_end(args);
+}
