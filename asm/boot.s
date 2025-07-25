9fbda9468c2fb673d51868095147fe8cfab189e3
iamwen1023 <ursula771023@gmail.com>
2025-07-25 16:08:38 +0200

    clean bss

--- asm/boot.s
+++ asm/boot.s
@@ -14,6 +14,19 @@ start:
     extern kernel_main
     extern gdt_load
     extern gdt_flush
+    extern __bss_start      ; Symbol from linker script: start of .bss
+    extern __bss_end        ; Symbol from linker script: end of .bss
+    
+    ; ------------------------------------------------------
+    ; Zero the .bss section (all uninitialized globals/statics)
+    ; ------------------------------------------------------
+    mov edi, __bss_start    ; Destination pointer (start of .bss)
+    mov ecx, __bss_end      ; End of .bss
+    sub ecx, __bss_start    ; ecx = size in bytes
+    shr ecx, 2              ; ecx = size in dwords (divide by 4)
+    xor eax, eax            ; Zero value
+    rep stosd               ; Fill ECX dwords at [EDI] with EAX (zero)
+    ; ------------------------------------------------------
     
     ; Set up stack
     mov esp, stack_top
